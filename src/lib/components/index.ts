0e5f6ca8b2c44b54e7b491a67b6e5cb28098815c
Luis Valero <lvalero@dexma.com>
2023-10-11 12:19:50 +0200

    feat(pagination): add Pagination component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -12,6 +12,7 @@ export * from './Grid';
 export * from './Icon';
 export * from './Input';
 export * from './Paragraph';
+export * from './Pagination';
 export * from './Progress';
 export * from './Result';
 export * from './Row';
