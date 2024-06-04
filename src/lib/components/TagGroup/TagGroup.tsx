fb885c3cb39fc0237b83a0708340010b94c49555
Cristian Martinez <cmartinez@dexma.com>
2024-06-04 19:09:23 +0200

    DASH-1989 | WIP fixes

--- src/lib/components/TagGroup/TagGroup.tsx
+++ src/lib/components/TagGroup/TagGroup.tsx
@@ -6,10 +6,11 @@ import { TagProps } from '@components';
 import { StyledTagGroup } from '@styles/TagGroup/StyledTagGroup';
 
 export const TagGroup = (props: { children: React.ReactElement<TagProps> | React.ReactElement<TagProps>[] }) => {
+    const { children, ...rest } = props;
     const th = useContext(ThemeContext) || theme;
     return (
-        <StyledTagGroup data-testid='tag-group' theme={th}>
-            {props.children}
+        <StyledTagGroup data-testid='tag-group' theme={th} {...rest}>
+            {children}
         </StyledTagGroup>
     );
 };
